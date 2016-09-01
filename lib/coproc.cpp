403f800d90c7b5933cee730ed04b853192f5d2f3
Christian Beer <christian.beer@aei.mpg.de>
2016-09-01 16:40:31 +0200

    Lib/Sched: remove superfluous ';'
    Those extra ';' produce warnings when using the "-std=gnu++11 -pedantic" flags because they are not conform with the C++11 standard.

--- lib/coproc.cpp
+++ lib/coproc.cpp
@@ -696,7 +696,7 @@ void COPROC_ATI::write_xml(MIOFILE& f, bool scheduler_rpc) {
     }
         
     f.printf("</coproc_ati>\n");
-};
+}
 #endif
 
 void COPROC_ATI::clear() {
@@ -905,7 +905,7 @@ void COPROC_INTEL::write_xml(MIOFILE& f, bool scheduler_rpc) {
     }
         
     f.printf("</coproc_intel_gpu>\n");
-};
+}
 #endif
 
 void COPROC_INTEL::clear() {
